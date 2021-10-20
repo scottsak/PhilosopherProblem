f8b491dd5989f0989169c57ab02d631164b9b968
scottsak <48599109+scottsak@users.noreply.github.com>
2021-10-20 00:44:52 -0700

    removed unnecessary classes

--- Philosopher.java
+++ Philosopher.java
@@ -9,5 +9,56 @@
 
 public class Philosopher implements Runnable
 {
- //Your code here
+
+    // The forks on either side of this Philosopher 
+    private Object leftFork;
+    private Object rightFork;
+
+    public Philosopher(Object leftFork, Object rightFork) {
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
+    }
+
+    // Member variables, standard constructor
+
+    private void doAction(String action) throws InterruptedException {
+        System.out.println(
+          Thread.currentThread().getName() + " " + action);
+        Thread.sleep(((int) (Math.random() * 100)));
+    }
+    // Member variables, methods defined earlier
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                
+                // thinking
+                doAction(System.nanoTime() + ": Thinking");
+                synchronized (leftFork) {
+                    doAction(
+                    System.nanoTime() 
+                        + ": Picked up left fork");
+                    synchronized (rightFork) {
+                        // eating
+                        doAction(
+                        System.nanoTime() 
+                            + ": Picked up right fork - eating"); 
+                        
+                        doAction(
+                        System.nanoTime() 
+                            + ": Put down right fork");
+                    }
+                    
+                    // Back to thinking
+                    doAction(
+                    System.nanoTime() 
+                        + ": Put down left fork. Back to thinking");
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return;
+        }
+    }
 }
